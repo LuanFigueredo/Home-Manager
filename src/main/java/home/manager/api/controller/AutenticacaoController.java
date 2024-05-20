0ffccaf310a7b62c2a712a9032e509c3195910c0
Luan Figueredo <luanfigueredodasilva@gmail.com>
2024-05-19 22:32:49 -0300

    fix: ajustando crossOrigin para rota 5173

--- src/main/java/home/manager/api/controller/AutenticacaoController.java
+++ src/main/java/home/manager/api/controller/AutenticacaoController.java
@@ -13,14 +13,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/login")
+@CrossOrigin(origins = "http://localhost:5173")
 @Log4j2
 public class AutenticacaoController {
     @Autowired
